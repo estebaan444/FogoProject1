3851075b4d084eb1c09f9b15981cbca6d77857d0
Marc Gutiérrez <imarlex45@gmail.com>
2023-04-19 18:28:58 +0200

    Recipe on user profile

--- app/src/main/java/com/estebi/fogo1/ui/goToUser/GoToUserActivity.kt
+++ app/src/main/java/com/estebi/fogo1/ui/goToUser/GoToUserActivity.kt
@@ -1,27 +1,22 @@
 package com.estebi.fogo1.ui.goToUser
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.estebi.fogo1.R
 import com.estebi.fogo1.models.Posts
 import com.estebi.fogo1.repository.auth.AuthRepository.Companion.db
 import com.estebi.fogo1.repository.posts.PostsRepository
 import com.estebi.fogo1.ui.goToUser.adapter.GoToUserAdapter
-import com.estebi.fogo1.ui.search.SearchFragment
 import com.estebi.fogo1.ui.search.SearchViewModel.Companion.getUserEmailKey
-import com.estebi.fogo1.ui.user.adapter.MyPostsAdapter
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.core.Context
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.RequestCreator
 
 class GoToUserActivity : AppCompatActivity() {
     private val goToUserAdapter = GoToUserAdapter()
@@ -32,6 +27,7 @@ class GoToUserActivity : AppCompatActivity() {
         setContentView(R.layout.activity_go_to_user)
         var userNameTxt = findViewById<TextView>(R.id.userNameOfSelect)
         val userImgSelect = findViewById<ImageView>(R.id.userImgOfSelect)
+        val context: GoToUserActivity = this
 
         val goBackButton = findViewById<ImageView>(R.id.goBackSearchFragment)
         goBackButton.setOnClickListener {
@@ -59,7 +55,17 @@ class GoToUserActivity : AppCompatActivity() {
         myPostsRv.adapter = goToUserAdapter
         goToUserAdapter.setItemListener(object : GoToUserAdapter.onItemClickListener {
             override fun onItemClick(posts: Posts) {
-                Toast.makeText(this@GoToUserActivity, posts.postDesc, Toast.LENGTH_SHORT).show()
+                val postDesc = posts.postDesc
+                val builder = AlertDialog.Builder(context)
+                with(builder)
+                {
+                    setTitle("The recipe of ${posts.titlePost}")
+                    setMessage(postDesc)
+                    setPositiveButton("OK") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    show()
+                }
             }
         })
         observePosts()
